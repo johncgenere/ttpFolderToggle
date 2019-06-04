34945b85a48086dcf00286364fda1c7d8b4f2ed6
John Genere <johncgenere@gmail.com>
2019-06-04 14:05:00 -0400

    Added folder and files

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <h1> Hello World! </h1>
-        </header>
+        <div className="App-header">
+          <button className="ui white button"><i className="folder icon"></i> Home </button>
+          <ul>
+            <li> <i className="file icon"></i> File 1 </li>
+            <li> <i className="file icon"></i> File 2 </li>
+            <li> <i className="file icon"></i> File 3 </li>
+          </ul>
+        </div>
       </div>
     );
   }
