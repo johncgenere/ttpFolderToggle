a91f8ce154a3d43b396a0cae664e521d91cf9e1e
johncgenere <johncgenere@gmail.com>
2019-06-04 14:06:27 -0400

    Added Folder and Files (#1)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="App-header">
+          <button className="ui white button"><i className="folder icon"></i> Home </button>
+          <ul>
+            <li> <i className="file icon"></i> File 1 </li>
+            <li> <i className="file icon"></i> File 2 </li>
+            <li> <i className="file icon"></i> File 3 </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
