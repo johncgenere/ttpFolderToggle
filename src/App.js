68a34e04e207ffc6c2994e5ddbb7623712264609
johncgenere <johncgenere@gmail.com>
2019-06-04 14:19:47 -0400

    Added the Toggle Button With Logic (#2)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,46 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+      super(props);
+
+      this.state = {
+        open: true
+      }
+
+      this.onToggleClick = this.onToggleClick.bind(this);
+  }
+
+  onToggleClick = (event) => {
+    if(this.state.open)
+      this.setState({open: false});
+    else
+      this.setState({open: true});
+  }
+
   render(){
-    return (
-      <div className="App">
-        <div className="App-header">
-          <button className="ui white button"><i className="folder icon"></i> Home </button>
-          <ul>
-            <li> <i className="file icon"></i> File 1 </li>
-            <li> <i className="file icon"></i> File 2 </li>
-            <li> <i className="file icon"></i> File 3 </li>
-          </ul>
+    if(this.state.open){
+      return (
+        <div className="App">
+          <div className="App-header">
+            <button className="ui white button" style={{marginBottom: '2%'}}><i className="folder open icon"></i> Home </button>
+              <p><i className="caret right icon"></i><i className="file icon"></i> File1</p>
+              <p><i className="caret right icon"></i><i className="file icon"></i> File2</p>
+              <p><i className="caret right icon"></i><i className="file icon"></i> File3</p>
+            <button className="ui red button" onClick={this.onToggleClick}> Toggle </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <button className="ui white button" style={{marginBottom: '2%'}}><i className="folder icon"></i> Home </button>
+            <button className="ui red button" onClick={this.onToggleClick}> Toggle </button>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
